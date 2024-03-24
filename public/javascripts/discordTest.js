021621978a2599f4b2bac56222aa78ecaf7280fd
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2024-03-23 20:28:56 -0400

    Improve code to minimize overhead to send a message, and fix bugs

--- public/javascripts/discordTest.js
+++ public/javascripts/discordTest.js
@@ -6,36 +6,63 @@ var GatewayIntentBits = require('discord.js').GatewayIntentBits;
 const TOKEN = process.env.TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 
+var client; //Holds the Discord client
+var channel; //Holds the channel to send messages to
+
 /*
     Test script to establish basic Discord bot functionality.
-
-    Bug: Sometimes first page load does not cause message to send, but subsequent page loads do.
-    Bug: Sometimes message does not send at all.
 */
 
 async function main(){
+    const message = new Date().toString();
+
+    if(!client){
+        //If the client does not exist, get the client and channel then send a message
+        await getClient();
+        await getChannel(client);
+        await sendMessage(channel, message);
+    }else if(!channel){
+        //If the channel does not exist, get the channel
+        await getChannel(client);
+        await sendMessage(channel, message);
+    }
+    else{
+        //If the client and channel exist, send a message
+        await sendMessage(channel, message);
+    }
+}
+
+async function getClient(){
     //Create a new client
-    const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+    client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
     //Login to the client
     await client.login(TOKEN);
 
     //When the client is ready, log a message to the console and send a message to the channel
     client.once('ready', () => {
-        console.log('Client is Ready!');
+        console.log('Client is Ready!');    
+        getChannel();
+    });
 
+    this.client = client;
+}
+
+async function getChannel(client){
         //Get the channel by ID
-        const channel = client.channels.cache.get(CHANNEL_ID);
+        channel = client.channels.cache.get(CHANNEL_ID);
 
         //If I remove this line everything breaks, so I'm keeping it here for now. Something to do with concurrency.
         console.log(channel);
         
-        // Send a basic message
-        channel.send('hello5!')
-        .then(message => console.log(`Sent message: ${message.content}`))
-        .catch(console.error);
-    });
-    
+        this.channel = channel;
+}
+
+async function sendMessage(channel, message){
+    //Send a message to the channel
+    channel.send(message)
+    .then(message => console.log(`Sent message: ${message.content}`))
+    .catch(console.error);
 }
 
 module.exports = {main};
