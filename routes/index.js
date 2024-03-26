d2739226100996496ea3bf9a8299caa4d09025fb
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2024-03-26 18:31:26 -0400

    Improve bot implementation, add form for sending a message

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var discord = require('../public/javascripts/discordTest.js');
+var DiscordBot = require('../public/javascripts/discordBot.js');
 
+var bot; //Holds our bot
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+  if(!bot){
+    bot = new DiscordBot();
+    //Wait until DiscordBot is constructed and client and channel are set before sending a message
+    await bot.init();
+  }
+  res.render('index', { title: 'Express' });
+});
 
-  await discord.main();
+//Accept a post request and send a message to the Discord channel
+router.post('/sendmessage', async function(req, res, next) {
+  if(!bot){
+    bot = new DiscordBot();
+    //Wait until DiscordBot is constructed and client and channel are set before sending a message
+    await bot.init();
+  }else{
+    bot.sendMessage(req.body.message);
+  }
 
   res.render('index', { title: 'Express' });
 });
